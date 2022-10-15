2c70de3b14a0bad82fcdd2588cd6e6b07d7f39df
tajbaba999 <tajbaba153@gmail.com>
2022-10-15 20:23:25 +0530

    Changes in even odd

---
+++ Even_or_Odd_number_In_C.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+void main()
+{
+    int num;
+    
+    printf("Enter the number =");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("%d is the Even Number\n",num);
+    }
+    else
+    {
+        printf("%d is the Odd Number\n",num);
+    } 
+ 
+    
+}
