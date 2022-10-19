9286722aef9ebdba25ae9a46c3c4bf75f6d47631
Anonymous-Technopath <93417822+Anonymous-Technopath@users.noreply.github.com>
2022-10-19 09:25:47 +0530

    Updated insertionSort.c
    Made some changes

--- insertionSort.c
+++ insertionSort.c
@@ -2,22 +2,18 @@
 #include <math.h>
 #include <stdio.h>
 
-/* Function to sort an array
-using insertion sort*/
+/* Function to sort an array using insertion sort*/
 void insertionSort(int arr[], int n)
 {
 	int i, key, j;
-	for (i = 1; i < n; i++)
-	{
+	for (i = 1; i < n; i++) {
 		key = arr[i];
 		j = i - 1;
 
-		/* Move elements of arr[0..i-1],
-		that are greater than key,
-		to one position ahead of
-		their current position */
-		while (j >= 0 && arr[j] > key)
-		{
+		/* Move elements of arr[0..i-1], that are
+		greater than key, to one position ahead
+		of their current position */
+		while (j >= 0 && arr[j] > key) {
 			arr[j + 1] = arr[j];
 			j = j - 1;
 		}
@@ -25,8 +21,7 @@ void insertionSort(int arr[], int n)
 	}
 }
 
-// A utility function to print
-// an array of size n
+// A utility function to print an array of size n
 void printArray(int arr[], int n)
 {
 	int i;
@@ -35,10 +30,10 @@ void printArray(int arr[], int n)
 	printf("\n");
 }
 
-// Driver code
+/* Driver program code to test insertion sort */
 int main()
 {
-	int arr[] = {12, 11, 13, 5, 6};
+	int arr[] = { 1, 90, 2, 22, 8 ,15 , 19};
 	int n = sizeof(arr) / sizeof(arr[0]);
 
 	insertionSort(arr, n);
